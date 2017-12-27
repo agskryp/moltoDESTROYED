c884cdde5f20e02ea3d4adf187c62fbe96b31c47
Andrew Skrypnyk <a.g.skryp@gmail.com>
2017-12-27 17:21:49 -0500

    Add front page template

---
+++ page-templates/page-front.php
@@ -0,0 +1,29 @@
+<?php
+  /* Template Name: Front Page */
+
+  get_header(); 
+?>
+
+<main>
+  <p>
+    Yaaay! Welcome to moltoDESTROYED!! A silly little web comic featuring fifteen eccentric characters and the special moments they share!
+  </p>
+
+  <p>
+    Now don't be afraid to jump straight into the latest comic strip, or if you're a first timer, start at the beginning and watch the evolving changes in illustration and major decline of humour between each comic!
+  </p>
+
+  <p>
+    Feel free to like and/or follow us on the many different social networks were connected with like FaceBook and Twitter and Google+ to instantly receive the latest comic, there's also a Tumblr and Instagram page for images of various drawings and original sketches. If you aren't about the social scene, grab the RSS Feed.
+  </p>
+
+  <p>
+    If you're curious to know more about moltoDESTROYED, the about page has a couple of sentences explaining the who and whats of the website.
+  </p>
+</main>
+
+<?php 
+  //get_sidebar();
+
+  get_footer(); 
+?>
