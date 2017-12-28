478d954e69277b6de4654c8b148349801a64de9d
Andrew Skrypnyk <a.g.skryp@gmail.com>
2017-12-28 16:44:44 -0500

    Add bootstrap CDN, renamed author

--- footer.php
+++ footer.php
@@ -1,31 +1,35 @@
 <?php
-/**
- * The template for displaying the footer
- *
- * Contains the closing of the #content div and all content after.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package moltodestroyed
- */
-
+  /**
+   * The template for displaying the footer
+   * Contains the closing of the #content div and all content after.
+   *
+   * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+   * @package moltodestroyed
+   */
 ?>
 
-	</div><!-- #content -->
+</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'moltodestroyed' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'moltodestroyed' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'moltodestroyed' ), 'moltodestroyed', '<a href="http://agskryp.com">Andrew Skrypnyk</a>' );
-			?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+<footer id="colophon" class="site-footer">
+  <div class="site-info">
+    <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'moltodestroyed' ) ); ?>">
+      <?php
+        /* translators: %s: CMS name, i.e. WordPress. */
+        printf( esc_html__( 'Proudly powered by %s', 'moltodestroyed' ), 'WordPress' );
+      ?>
+    </a>
+    
+    <span class="sep"> | </span>
+    
+    <?php
+      /* translators: 1: Theme name, 2: Theme author. */
+      printf( esc_html__( 'Theme: %1$s by %2$s.', 'moltodestroyed' ),
+             'moltodestroyed', '<a href="http://agskryp.com">AGSkryp</a>'
+            );
+    ?>
+  </div><!-- .site-info -->
+</footer><!-- #colophon -->
+
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
