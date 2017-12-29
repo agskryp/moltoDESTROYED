6827f3d6b7565c645e4fe3397c67c43e0ca19280
Andrew Skrypnyk <a.g.skryp@gmail.com>
2017-12-29 16:54:13 -0500

    Create about page template

---
+++ page-templates/page-about.php
@@ -0,0 +1,28 @@
+<?php
+  /* Template Name: About Page */
+
+  get_header(); 
+?>
+
+<main class="container">
+  <div class="row">
+    <div class="col-xs-12">
+      <h2>About moltoDESTROYED</h2>
+      
+      <p>
+        moltoDESTROYED is a simple, silly, and monochromatic web comic featuring fifteen eccentric characters and various forms of humour.
+      </p>
+
+      <p>
+        moltoDESTROYED &amp; moltodestroyed.com is written, illustrated, created, and produced by Molto D. Stroyed. Comments and criticism about the comic can be e-mailed here, feedback and issues with the website can be addressed to the webmaster.
+      </p>
+      
+      <p>
+        The cast and characters of moltoDESTROYED are entirely fictional and do not represent any fruit, vegetable, snack, animal, party supply or person living or dead.
+      </p>
+    </div>
+  </div>
+</main>
+
+<?php
+  get_footer(); 
