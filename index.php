81964b4a19911c01d8a79d9aa8277a805f7e8f62
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-01-22 13:53:16 -0500

    Edit Index page (Blog excerpt) to have columns on desktop

--- index.php
+++ index.php
@@ -15,49 +15,47 @@
 ?>
 
 <div id="primary" class="content-area">
-  <main id="main" class="site-main">
-    <div class="container">
-      <div class="row">
-        <div class="col-xs-12 col-sm-8">
-          <?php if ( have_posts() ) : ?>
-            <?php if ( is_home() && ! is_front_page() ) : ?>
-              <header>
-                <h1 class="page-title screen-reader-text">
-                  <?php single_post_title(); ?>
-                </h1>
-              </header> 
-            <?php endif; ?>
+  <main id="main" class="site-main container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8">
+        <?php if ( have_posts() ) : ?>
+          <?php if ( is_home() && ! is_front_page() ) : ?>
+            <header>
+              <h1 class="page-title screen-reader-text">
+                <?php single_post_title(); ?>
+              </h1>
+            </header> 
+          <?php endif; ?>
 
-          <?php
-            /* Start the Loop */
-            while ( have_posts() ) :
+        <?php
+          /* Start the Loop */
+          while ( have_posts() ) :
 
-            the_post();
+          the_post();
 
-            /*
-             * Include the Post-Format-specific template for the content.
-             * If you want to override this in a child theme, then include a file
-             * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-             */
-            get_template_part( 'template-parts/content', get_post_format() );
+          /*
+           * Include the Post-Format-specific template for the content.
+           * If you want to override this in a child theme, then include a file
+           * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+           */
+          get_template_part( 'template-parts/content', get_post_format() );
 
-            endwhile;
-          ?>
+          endwhile;
+        ?>
 
-          <?php
-            the_posts_navigation();
+        <?php
+          the_posts_navigation();
 
-            else :
+          else :
 
-            get_template_part( 'template-parts/content', 'none' );
+          get_template_part( 'template-parts/content', 'none' );
 
-            endif;
-          ?>  
-        </div>
-        
-        <?php require get_template_directory() . '/partials/sidebar.php'; ?>
+          endif;
+        ?>  
       </div>
-    </div>
+
+      <?php require get_template_directory() . '/partials/sidebar.php'; ?>
+    </div>    
   </main><!-- #main -->
 </div><!-- #primary -->
 
