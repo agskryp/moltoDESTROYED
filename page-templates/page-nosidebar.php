a4d477b85439e5534ac2d86e592c57b43f0717cf
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-01-22 13:53:38 -0500

    Add No Sidebar page template

---
+++ page-templates/page-nosidebar.php
@@ -0,0 +1,32 @@
+<?php
+  /* Template Name: No Sidebar */
+
+  get_header(); 
+?>
+
+  <div id="primary" class="content-area">
+    <main id="main" class="site-main container">
+      <div class="row">
+        <div class="col-xs-12">
+          <?php
+			while ( have_posts() ) : 
+              the_post();
+
+              get_template_part( 'template-parts/content', 'page' );
+
+              // If comments are open or we have at least one comment, load up the comment template.
+              if ( comments_open() || get_comments_number() ) :
+                  comments_template();
+              endif;
+
+			endwhile; // End of the loop.
+		  ?>
+        </div>
+      </div>
+    </main>
+    <!-- #main -->
+  </div>
+  <!-- #primary -->
+
+<?php
+get_footer();
