bbbe4089057004f18befef4c4fd01066b1b1ba86
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-01-28 17:26:21 -0500

    Create posted on function for comics

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -39,6 +39,30 @@ if ( ! function_exists( 'moltodestroyed_posted_on' ) ) :
 		echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
 
 	}
+
+    function moltodestroyed_comic_posted_on() {
+		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+  
+		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+		}
+
+		$time_string = sprintf( $time_string,
+			esc_attr( get_the_date( 'c' ) ),
+			esc_html( get_the_date() ),
+			esc_attr( get_the_modified_date( 'c' ) ),
+			esc_html( get_the_modified_date() )
+		);
+
+		$posted_on = sprintf(
+			/* translators: %s: post date. */
+			esc_html_x( 'Posted on %s', 'post date', 'moltodestroyed' ),
+			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+		);
+
+		echo '<span class="posted-on">' . $posted_on . '</span>'; // WPCS: XSS OK.
+
+	}
 endif;
 
 if ( ! function_exists( 'moltodestroyed_entry_footer' ) ) :
