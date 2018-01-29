87ac0a0003fa5c8022d7b13240f06ddf2a33ec40
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-01-29 13:10:07 -0500

    Clean up front page code

--- page-templates/page-front.php
+++ page-templates/page-front.php
@@ -4,34 +4,39 @@
   get_header(); 
 ?>
 
-<main class="container">
+<div class="container">
   <div class="row">
     <div class="col-xs-12 col-sm-8 text-center">
-    
-        <?php
-      $wp_query = new WP_Query ( array( 'post_type' => 'comics', 'posts_per_page' => 1 ) );
-      
-            while ( have_posts() ) : the_post();
+      <div id="primary" class="content-area">
+        <main id="main" class="site-main">
+          <?php
+            $wp_query = new WP_Query ( array( 
+              'post_type'       => 'comics',
+              'posts_per_page'  => 1 
+            ) );
 
-            get_template_part( 'template-parts/content', get_post_type() );
+            while ( have_posts() ) :
+              the_post();
 
-            the_post_navigation();
+              get_template_part( 'template-parts/content', get_post_type() );
 
-            // If comments are open or we have at least one comment, load up the comment template.
-            if ( comments_open() || get_comments_number() ) :
-                comments_template();
-            endif;
+              the_post_navigation();
+
+              // If comments are open or we have at least one comment, load up the comment template.
+              if ( comments_open() || get_comments_number() ) :
+                  comments_template();
+              endif;
 
             // End of the loop.
             endwhile; 
           ?>
-      
-      </div>
-        <?php require get_template_directory() . '/partials/sidebar.php'; ?>    
-      
+        </main> <!-- #main -->
+      </div> <!-- #primary -->
+    </div>
+    
+    <?php require get_template_directory() . '/partials/sidebar.php'; ?>      
   </div>
-</main>
+</div>
 
 <?php 
-
   get_footer(); 
