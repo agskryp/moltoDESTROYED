8d5df3f618e274a325730d07ee0dc90ab3185ae0
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-01-29 11:23:35 -0500

    Add script for recent comics

--- sidebar.php
+++ sidebar.php
@@ -14,4 +14,23 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 
 <aside id="secondary" class="widget-area">
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
+  
+  <h2>Recent Comics</h2>
+   
+  <ul>
+<?php
+    $recent_posts = wp_get_recent_posts(array('post_type'=>'comics', 'posts_per_page' => 5 ));
+    
+
+    
+    foreach( $recent_posts as $recent ){
+        echo '<li><a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'</a> </li> ';
+    }
+?>
+</ul>
+  
+
+  
+  
+  <h2>Follow Us</h2>
 </aside><!-- #secondary -->
