3652a9b8fc8d61c103bce1f6f8d609b2af4ddc50
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-01-29 10:29:49 -0500

    Clean archive-comics.php

--- archive-comics.php
+++ archive-comics.php
@@ -9,11 +9,11 @@
 
 get_header(); ?>
 
-<div id="primary" class="content-area">
-  <main id="main" class="site-main">
-    <div class="container">
-      <div class="row">
-        <div class="col-xs-12 col-sm-8 text-center">
+<div class="container">
+  <div class="row">
+    <div class="col-xs-12 col-sm-8 text-center">
+      <div id="primary" class="content-area comic-archive">
+        <main id="main" class="site-main">    
           <?php if ( have_posts() ) : ?>
             <header>
               <h1>Comic Archive</h1>
@@ -21,32 +21,23 @@ get_header(); ?>
 
             <?php
               /* Start the Loop */
-              while ( have_posts() ) : the_post();
-
-                /*
-                 * Include the Post-Format-specific template for the content.
-                 * If you want to override this in a child theme, then include a file
-                 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-                 */
+              while ( have_posts() ) :
+                the_post();
                 get_template_part( 'template-parts/content-comic-archive' );
 
               endwhile;
 
-            the_posts_navigation();
-
-            else :
-
-              get_template_part( 'template-parts/content', 'none' );
-
+              the_posts_navigation();
+          
             endif;
-          ?>
-        </div>
-
-        <?php require get_template_directory() . '/partials/sidebar.php'; ?>
-      </div>
+          ?> 
+        </main> <!-- #main -->
+      </div> <!-- #primary -->
     </div>
-  </main> <!-- #main -->
-</div> <!-- #primary -->
+
+    <?php require get_template_directory() . '/partials/sidebar.php'; ?>
+  </div>
+</div>
 
 <?php
 get_footer();
