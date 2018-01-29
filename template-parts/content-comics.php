2f4966d838fe3baf300a14adef835e7a091ab48a
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-01-29 10:29:25 -0500

    Clean content-comics.php

--- template-parts/content-comics.php
+++ template-parts/content-comics.php
@@ -11,55 +11,27 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
   <header class="entry-header">
-      <?php
-      if ( is_singular() ) :
-          the_title( '<h1 class="entry-title">', '</h1>' );
-      else :
-          the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-      endif;
-
-      if ( 'post' === get_post_type() ) : ?>
-      <div class="entry-meta">
-          <?php moltodestroyed_posted_on(); ?>
-      </div> <!-- .entry-meta -->
-      <?php endif; ?>
+    <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
   </header> <!-- .entry-header -->
+  
+  <div class="entry-content comic-strip">
+    <?php
+      $image = get_field( 'comic_strip' );
 
-	<?php moltodestroyed_post_thumbnail(); ?>
+      if( !empty( $image ) ): 
+    ?>
+      <img src="<?php echo $image['url']; ?>" alt="<?php echo the_title(); ?>" />
+    
+      <div class="entry-meta">
+        <?php moltodestroyed_comic_posted_on(); ?>
+      </div> <!-- .entry-meta -->
+    <?php 
+      endif; 
 
-	<div class="entry-content">
-		<?php
-			the_content( sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'moltodestroyed' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				get_the_title()
-			) );
-      
- 
-
-$image = get_field('comic_strip');
-
-if( !empty($image) ): ?>
-
-	<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
-
-<?php endif; 
-
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'moltodestroyed' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php moltodestroyed_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-<?php the_ID(); ?> -->
+      wp_link_pages( array(
+          'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'moltodestroyed' ),
+          'after'  => '</div>',
+      ) );
+	?>
+  </div> <!-- .entry-content -->
+</article> <!-- #post-<?php the_ID(); ?> -->
