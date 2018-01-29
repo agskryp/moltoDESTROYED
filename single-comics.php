2573b83f48cdb3cf96e2c323d4fcd460c4cabb10
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-01-29 15:25:08 -0500

    Fix primary / secondary structure for single-comics.php

--- single-comics.php
+++ single-comics.php
@@ -9,33 +9,34 @@
   get_header(); 
 ?>
 
-<div id="primary" class="content-area">
-  <main id="main" class="site-main">
-    <div class="container">
-      <div class="row">
-        <div class="col-xs-12 col-sm-8 text-center">
+<div class="container">
+  <div class="row">
+    <div class="col-xs-12 col-sm-8 text-center">
+      <div id="primary" class="content-area">
+        <main id="main" class="site-main">
           <?php
-            while ( have_posts() ) : the_post();
+            while ( have_posts() ) :
+              the_post();
 
-            get_template_part( 'template-parts/content', get_post_type() );
+              get_template_part( 'template-parts/content', get_post_type() );
 
-            the_post_navigation();
+              the_post_navigation();
 
-            // If comments are open or we have at least one comment, load up the comment template.
-            if ( comments_open() || get_comments_number() ) :
-                comments_template();
-            endif;
+              // If comments are open or we have at least one comment, load up the comment template.
+              if ( comments_open() || get_comments_number() ) :
+                  comments_template();
+              endif;
 
             // End of the loop.
             endwhile; 
           ?>
-        </div>
-      
-        <?php require get_template_directory() . '/partials/sidebar.php'; ?>        
-      </div>
+        </main> <!-- #main -->
+      </div> <!-- #primary -->    
     </div>
-  </main><!-- #main -->
-</div><!-- #primary -->
+    
+    <?php require get_template_directory() . '/partials/sidebar.php'; ?>        
+  </div>
+</div>
 
 <?php
   get_footer();
