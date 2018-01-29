c9078807999c2ea2a179cca7bdbc2f296e14c5ed
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-01-29 15:21:30 -0500

    Merge content template into single file

--- archive-comics.php
+++ archive-comics.php
@@ -11,8 +11,8 @@ get_header(); ?>
 
 <div class="container">
   <div class="row">
-    <div class="col-xs-12 col-sm-8 text-center">
-      <div id="primary" class="content-area comic-archive">
+    <div class="col-xs-12 col-sm-8">
+      <div id="primary" class="content-area comic-archive text-center">
         <main id="main" class="site-main">    
           <?php if ( have_posts() ) : ?>
             <header>
@@ -21,16 +21,17 @@ get_header(); ?>
 
             <?php
               /* Start the Loop */
-              while ( have_posts() ) :
-                the_post();
-                get_template_part( 'template-parts/content-comic-archive' );
-
-              endwhile;
-
-              the_posts_navigation();
-          
-            endif;
-          ?> 
+              while ( have_posts() ) : the_post(); // required to prevent looping
+            ?>
+              <ul>
+                <li>
+                  <?php
+                    the_title( '<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a>' );
+                  ?>    
+                </li>
+              </ul>
+            <?php endwhile; ?>
+          <?php endif; ?> 
         </main> <!-- #main -->
       </div> <!-- #primary -->
     </div>
