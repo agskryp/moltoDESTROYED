ebfbb546093d0e28ba11c8f41f5b97faa7548d48
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-01-29 14:17:36 -0500

    Clean up sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -7,30 +7,32 @@
  * @package moltodestroyed
  */
 
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+  if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 	return;
-}
+  }
 ?>
 
 <aside id="secondary" class="widget-area">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
-  
-  <h2>Recent Comics</h2>
-   
-  <ul>
-<?php
-    $recent_posts = wp_get_recent_posts(array('post_type'=>'comics', 'posts_per_page' => 5 ));
-    
+  <?php dynamic_sidebar( 'sidebar-1' ); ?>
 
-    
-    foreach( $recent_posts as $recent ){
-        echo '<li><a href="' . get_permalink($recent["ID"]) . '" title="Look '.esc_attr($recent["post_title"]).'" >' .   $recent["post_title"].'</a> </li> ';
-    }
-?>
-</ul>
-  
+  <section class="widget">
+    <h2>Recent Comics</h2>
 
-  
-  
-  <h2>Follow Us</h2>
-</aside><!-- #secondary -->
+    <ul>
+      <?php
+        $recent_posts = wp_get_recent_posts( array( 
+          'post_type'      => 'comics',
+          'posts_per_page' => 5 
+        ) );
+
+        foreach( $recent_posts as $recent ) {
+          echo '<li><a href="' . get_permalink( $recent["ID"] ) . '" title="Look '.esc_attr( $recent["post_title"] ).'" >' .   $recent["post_title"].'</a> </li> ';
+        }
+      ?>
+    </ul>
+  </section>
+
+  <section class="widget">
+    <h2>Follow Us</h2>
+  </section>
+</aside> <!-- #secondary -->
