684580e147ded609c7d6cb35df41e5ef66751da2
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-02-16 19:34:26 -0500

    Use srcset for site banner

--- partials/site-banner.php
+++ partials/site-banner.php
@@ -1,1 +1,6 @@
- <img src="<?php echo get_template_directory_uri(); ?>/images/banner/molto-banner-template-sm.png">
+<img 
+     src="<?php echo get_template_directory_uri(); ?>/images/banner/molto-banner-template-xs.png"
+     srcset="<?php echo get_template_directory_uri(); ?>/images/banner/molto-banner-template-sm.png 480w,
+             <?php echo get_template_directory_uri(); ?>/images/banner/molto-banner-template-md.png 768w,"
+     alt="moltoDESTROYED Banner"
+     style="max-height: 190px;">
