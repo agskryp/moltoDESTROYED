d065acced9fca0cea4eab6e5a497231bd3477dd0
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-04-01 18:13:34 -0400

    Buffer php output, random comic link

--- partials/comic-navigation.php
+++ partials/comic-navigation.php
@@ -1,41 +1,73 @@
-<?php global $post; ?>
+<?php 
+  global $post; 
+
+  ob_start();
+?>
 
 <div class="comic-navigation-container">
   <nav class="comic-navigation">
+    <?php 
+      $first = get_posts( array(
+        'numberposts' => 1,
+        'order'       => 'asc',
+        'post_type'   => 'comics'
+      ) );
     
-     <?php 
+      $first_url = get_permalink( $first[0] -> ID );
     
-    $first = get_posts(array('numberposts' => 1,  'order' => 'asc', 'post_type'      => 'comics',));
-$url = get_permalink($first[0]->ID);
+      if ( $post -> ID !== $first[0] -> ID ) {
+        echo "<a href='" . $first_url . "'> < < < </a>";
+      }
+    
+    
+     
+      $previous = get_previous_post_link(
+		'%link', ' &lt; ', false, '', 'category'
+	  );
+    
+      echo $previous;
+    
     
 
-    if ( $post->ID !== $first[0]->ID ) {
-echo "<a href='" . $url . "'> < < < </a>";
-    }
-  ?>
+      $random = get_posts( array(
+        'numberposts' => 1,
+        'orderby'     => 'rand',
+        'post_type'   => 'comics',
+        'exclude'     => $post -> ID
+      ) );
+
     
-  <?php $previous = get_previous_post_link(
-		'%link', ' &lt; ', false, '', 'category'
-	);
+      $random_url = get_permalink( $random[0] -> ID );
+      
+      echo "<a href='" . $random_url . "'> ? ? ? </a>";
+      
+     
     
-    echo $previous;
+      $next = get_next_post_link(
+        '%link', ' &gt; ', false, '', 'category'
+      );
+    
+      echo $next;
+    
+      $latest = get_posts( array(
+        'numberposts' => 1,
+        'order'       => 'desc',
+        'post_type'   => 'comics'
+      ) );
+    
+      $latest_url = get_permalink( $latest[0] -> ID );
+    
+      if ( $post -> ID !== $latest[0] -> ID ) {
+        echo "<a href='" . $latest_url . "'> > > > </a>";
+      }
     ?>
-    
-    
-    <a href="#">???</a>
-    
-      <?php $next = get_next_post_link(
-		'%link', ' &gt; ', false, '', 'category'
-	);
-    
-    echo $next;
-    ?>
-    
-  
-    <?php  $latest = get_posts(array('numberposts' => 1,  'post_type'      => 'comics',));
-$url = get_permalink($latest[0]->ID);
-echo "<a href='" . $url . "'> > > > </a>";
-  ?>
-    
   </nav>
-  </div>
+</div>
+
+<?php
+
+$comic_navigation = ob_get_contents();
+
+ob_end_flush();
+
+return $comic_navigation;
