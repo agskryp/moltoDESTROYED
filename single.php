9f00a46123a707799728fc9a49f3d90734cbbe00
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-04-05 14:17:53 -0400

    Clean up single.php (blog posts)

--- single.php
+++ single.php
@@ -9,37 +9,30 @@
   get_header(); 
 ?>
 
- <div class="narrow-container">
-      <div class="row">
-        <div class="col-xs-12">
+<div class="narrow-container">
+  <div id="primary" class="content-area">
+    <main id="main" class="site-main">
+      <?php
+        while ( have_posts() ) :
+          the_post();
 
-<div id="primary" class="content-area">
-  <main id="main" class="site-main">
-   
-          <?php
-            while ( have_posts() ) : the_post();
+          get_template_part( 'template-parts/content', get_post_type() );
 
-            get_template_part( 'template-parts/content', get_post_type() );
+          the_post_navigation();
 
-            the_post_navigation();
+          // If comments are open or we have at least one comment, load up the comment template.
+          if ( comments_open() || get_comments_number() ) :
+            comments_template();
+          endif;
 
-            // If comments are open or we have at least one comment, load up the comment template.
-            if ( comments_open() || get_comments_number() ) :
-                comments_template();
-            endif;
+        // End of the loop.
+        endwhile; 
+      ?>
+    </main> <?php // #main // ?>
+  </div> <?php // #primary // ?>
 
-            // End of the loop.
-            endwhile; 
-          ?>
-     
-  </main> <!-- #main -->
-</div> <!-- #primary -->
-          
-             </div>
-      
-          <?php get_sidebar(); ?>     
-      </div>
-    </div>
+  <?php get_sidebar(); ?>  
+</div> <?php // .narrow-container // ?>
 
 <?php
   get_footer();
