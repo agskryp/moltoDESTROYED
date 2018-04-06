1ba0540464f4c4df785ecb9e6dc8381996f66bc2
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-04-06 14:58:32 -0400

    Clean up page-front.php template

--- page-templates/page-front.php
+++ page-templates/page-front.php
@@ -5,36 +5,32 @@
 ?>
 
 <div class="narrow-container">
-  <div class="row">
-    <div class="col-xs-12">
-      <div id="primary" class="content-area text-center">
-        <main id="main" class="site-main">
-          <?php
-            $wp_query = new WP_Query ( array( 
-              'post_type'       => 'comics',
-              'posts_per_page'  => 1 
-            ) );
+  <div id="primary" class="content-area text-center">
+    <main id="main" class="site-main">
+      <?php
+        $wp_query = new WP_Query ( array( 
+          'post_type'       => 'comics',
+          'posts_per_page'  => 1 
+        ) );
 
-            while ( have_posts() ) :
-              the_post();
+        while ( have_posts() ) :
+          the_post();
 
-              get_template_part( 'template-parts/content', get_post_type() );
+          get_template_part( 'template-parts/content', get_post_type() );
 
-              // If comments are open or we have at least one comment, load up the comment template.
-              if ( comments_open() || get_comments_number() ) :
-                  comments_template();
-              endif;
+          // If comments are open or we have at least one comment, load up the comment template.
+          if ( comments_open() || get_comments_number() ) :
+            comments_template();
+          endif;
 
-            // End of the loop.
-            endwhile; 
-          ?>
-        </main> <!-- #main -->
-      </div> <!-- #primary -->
-    </div>
+        // End of the loop.
+        endwhile; 
+      ?>
+    </main> <?php // #main // ?>
+  </div> <?php // #primary // ?>
     
-      <?php get_sidebar(); ?>     
-  </div>
-</div>
+  <?php get_sidebar(); ?>     
+</div> <?php // .narrow-container // ?>
 
 <?php 
   get_footer(); 
