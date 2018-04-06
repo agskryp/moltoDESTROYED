27bcc42691971d3be86026f540369f68e8dc83b8
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-04-06 13:37:52 -0400

    Clean up header.php

--- header.php
+++ header.php
@@ -9,78 +9,82 @@
 ?>
 
 <!doctype html>
+
 <html <?php language_attributes(); ?> >
-  <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-    
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<?php wp_head(); ?>
-  </head>
+  
+<head>
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <body <?php body_class(); ?> >
-    <div id="page" class="site">
-      <a class="skip-link screen-reader-text" href="#content">
-        <?php esc_html_e( 'Skip to content', 'moltodestroyed' ); ?>
-      </a>
+  <link rel="profile" href="http://gmpg.org/xfn/11">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <?php wp_head(); ?>
+</head>
 
-      <header id="masthead" class="site-header">
-        <div class="site-branding text-center">
-          <div class="container-fluid">
-            <div class="row">
-              <div class="col-xs-12">
-                <?php if ( is_front_page() ) : ?>
-                  <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-                    <h1 class="site-title sr-only">
-                      <?php bloginfo( 'name' ); ?>
-                    </h1>
+<body <?php body_class(); ?> >
+  <div id="page" class="site">
+    <a class="skip-link screen-reader-text" href="#content">
+      <?php esc_html_e( 'Skip to content', 'moltodestroyed' ); ?>
+    </a>
 
-                    <?php // the_header_image_tag(); ?>
-                    <?php require get_template_directory() . '/partials/site-banner.php'; ?>
-                  </a>
-                
-                <?php require get_template_directory() . '/partials/social-container.php'; ?>
-                
-                <?php else : ?>                
-                  <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-                    <p class="site-title sr-only">
-                      <?php bloginfo( 'name' ); ?>
-                    </p>
+    <header id="masthead" class="site-header">
+      <div class="site-branding text-center">
+        <div class="container-fluid">
+          <div class="col-xs-12"> <?php // Required for social-container // ?>
+            <?php if ( is_front_page() ) : ?>
+              <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+                <h1 class="site-title sr-only">
+                  <?php bloginfo( 'name' ); ?>
+                </h1>
 
-                    <?php // the_header_image_tag(); ?>
-                    <?php require get_template_directory() . '/partials/site-banner.php'; ?>
-                  </a>
-                
-                <?php require get_template_directory() . '/partials/social-container.php'; ?>
-                
-                <?php endif; ?>
-              </div>
-            </div>
-          </div>
-        </div><!-- .site-branding -->
+                <?php 
+                  // the_header_image_tag(); 
 
-        <nav id="site-navigation" class="main-navigation">
-          <div class="container">
-            <div class="row">
-              <div class="col-xs-12">          
-                <button class="menu-toggle main-menu-button" aria-controls="primary-menu" aria-expanded="false">
-                  <i class="fa fa-bars" aria-hidden="true"></i> 
-                  
-                  <?php esc_html_e( 'Main Menu', 'moltodestroyed' ); ?>
-                </button>
+                  require get_template_directory() . '/partials/site-banner.php';
+                ?>
+              </a>
+            <?php
+              require get_template_directory() . '/partials/social-container.php';
+
+              else :
+            ?>                
+              <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+                <p class="site-title sr-only">
+                  <?php bloginfo( 'name' ); ?>
+                </p>
 
                 <?php
-                  wp_nav_menu( array(
-                    'theme_location' => 'menu-1',
-                    'menu_id'        => 'primary-menu',
-                  ) );
-                ?>
-              </div>
-            </div>
-          </div>
-        </nav><!-- #site-navigation -->
-      </header><!-- #masthead -->
+                  // the_header_image_tag(); 
 
-      <div id="content" class="site-content">
+                  require get_template_directory() . '/partials/site-banner.php';
+                ?>
+              </a>
+            <?php
+              require get_template_directory() . '/partials/social-container.php';
+
+              endif;
+            ?>            
+          </div> <?php // .col-xs-12 // ?>
+        </div> <?php // .container-fluid // ?>
+      </div> <?php // .site-branding // ?>
+
+      <nav id="site-navigation" class="main-navigation">
+        <div class="container">        
+          <button class="menu-toggle main-menu-button" aria-controls="primary-menu" aria-expanded="false">
+            <?php esc_html_e( 'Main Menu', 'moltodestroyed' ); ?>
+            
+            <i class="fa fa-bars" aria-hidden="true"></i> 
+          </button>
+
+          <?php
+            wp_nav_menu( array(
+              'theme_location' => 'menu-1',
+              'menu_id'        => 'primary-menu',
+            ) );
+          ?>
+        </div> <?php // .container // ?>
+      </nav> <?php // #site-navigation // ?>
+    </header> <?php // #masthead // ?>
+
+    <div id="content" class="site-content">
