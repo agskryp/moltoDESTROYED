28e8cc69d63aab0401e529278ffeb079b7780ae0
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-04-11 20:35:40 -0400

    Add blog-posts class to index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 
 <div class="narrow-container">
   <div id="primary" class="content-area">
-    <main id="main" class="site-main">
+    <main id="main" class="site-main blog-posts">
       <?php if ( have_posts() ) : ?>
         <header>
           <h1 class="page-title screen-reader-text">
