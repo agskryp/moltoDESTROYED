dabb70edb613f83b2b3f6eb079876f1cbb26735b
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-04-15 21:05:28 -0400

    Add images, working php + javascript randomizer

--- partials/header-characters.php
+++ partials/header-characters.php
@@ -1,3 +1,55 @@
+<?php
+
+
+?>
+
+<script type="text/javascript">
+/*
+Random Image Script- By JavaScript Kit (http://www.javascriptkit.com) 
+Over 400+ free JavaScripts here!
+Keep this notice intact please
+*/
+function random_imglink(){
+
+<?php
+function returnimages() {
+   $dirname = get_template_directory() . "/images/characters/facing-left/";
+  $urlpath = get_template_directory_uri() . "/images/characters/facing-left/";
+ 
+   $extension = "\.(jpg|jpeg|png|gif|bmp)$";
+   $files = array();
+   $curimage=0;
+  
+   if($handle = opendir($dirname)) {
+       while(false !== ($file = readdir($handle))){
+//               if(eregi($extension, $file)){
+                 echo 'myimages[' . $curimage .']="' . $urlpath . $file . '";' . "\n";
+                 $curimage++;
+//               }
+       }
+
+       closedir($handle);
+   }
+   return($files);
+}
+
+echo "var myimages=new Array();" . "\n";
+returnimages();
+?>
+  
+  var indexToRemove = 0;
+var numberToRemove = 2;
+
+myimages.splice(indexToRemove, numberToRemove);
+  
+  console.log(myimages);
+
+var ry=Math.floor(Math.random()*myimages.length)
+document.write('<img src="'+myimages[ry]+'" border=0>')
+}
+random_imglink()
+</script>
+
 <div class="header-characters">
   <div class="large far-right"
        
@@ -36,4 +88,7 @@
 
 </div>
 -->
-  </div>         
+  </div> 
+
+
+
