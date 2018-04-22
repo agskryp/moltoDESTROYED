3bc04be295c73127e4efbcb8dcb08158061f0923
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-04-21 20:22:22 -0400

    Add more character images

--- footer.php
+++ footer.php
@@ -16,7 +16,7 @@
       <div class="row">
         <div class="col-xs-12 col-md-6 text-center">
            <img
-                class="footer-banner bottom-cushion"
+                class="footer-banner"
                 src="<?php echo get_template_directory_uri(); ?>/images/banner/molto-banner-template-inversed-xs.png"
                 srcset="<?php echo get_template_directory_uri(); ?>/images/banner/molto-banner-template-inversed-sm.png 360w"
                 alt="moltoDESTROYED Banner">
