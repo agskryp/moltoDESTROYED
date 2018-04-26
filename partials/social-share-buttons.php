e80ecbb0ffe1058180a55d349e8a69e9d95552db
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-04-26 16:39:56 -0400

    social share buttons styling wip

--- partials/social-share-buttons.php
+++ partials/social-share-buttons.php
@@ -1,38 +1,17 @@
-<script>
-
-function PopupCenter(url, title, w, h) {
-    // Fixes dual-screen position                         Most browsers      Firefox
-    var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : window.screenX;
-    var dualScreenTop = window.screenTop != undefined ? window.screenTop : window.screenY;
-
-    var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
-    var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
-
-    var left = ((width / 2) - (w / 2)) + dualScreenLeft;
-    var top = ((height / 2) - (h / 2)) + dualScreenTop;
-    var newWindow = window.open(url, title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
-
-    // Puts focus on the newWindow
-    if (window.focus) {
-        newWindow.focus();
-    }
-}
-
-</script>
-
-
-
-
-
-  <a
+<div class="text-center" style="margin-bottom: 1em;">
+  <a id="shareTwitter" class="social-share-button"
   href="https://twitter.com/intent/tweet?text=Check out &#39;<?php echo get_the_title(); ?>&#39; @ moltoDESTROYED: <?php echo get_permalink(); ?>"
-       target="popup" 
-  onclick="PopupCenter('','popup','600','300');"
+       target="popupTwitter" 
+  onclick="PopupCenter('','popupTwitter','600','300');"
      >
- <svg width="21" height="21" viewBox="0 0 24 24">
+ <svg width="25" height="25" viewBox="0 0 24 24">
                       <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/>
                     </svg>
 </a>
-
+  
+  
+  
+  
+</div>
 
 
