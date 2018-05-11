dfd258c4b2072d9fc0332ef21286b78f78509690
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-05-11 18:11:43 -0400

    Comment ads out for production site launch

--- partials/ads/top-of-main-area.php
+++ partials/ads/top-of-main-area.php
@@ -1,16 +1,18 @@
-<div class="ad-container">
-  <span class="text-center">
-    Advertisement
-  </span>
-
-  <ins class="adsbygoogle"
-       style="display:block"
-       data-ad-client="ca-pub-5942635838820429"
-       data-ad-slot="1990014616"
-       data-ad-format="auto">
-  </ins>
-
-  <script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
-  </script>
-</div>
+<?php
+//
+//<div class="ad-container">
+//  <span class="text-center">
+//    Advertisement
+//  </span>
+//
+//  <ins class="adsbygoogle"
+//       style="display:block"
+//       data-ad-client="ca-pub-5942635838820429"
+//       data-ad-slot="1990014616"
+//       data-ad-format="auto">
+//  </ins>
+//
+//  <script>
+//    (adsbygoogle = window.adsbygoogle || []).push({});
+//  </script>
+//</div>
