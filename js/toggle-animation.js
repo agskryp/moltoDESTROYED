1e8a123f8b8d4a8658ea33be1bedc9ea651572d8
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-06-16 00:53:22 -0400

    Toggle between view and close button text

--- js/toggle-animation.js
+++ js/toggle-animation.js
@@ -6,6 +6,16 @@ var jQuery;
   } );
   
   $( '#textified-toggle' ).click( function () {
+    var textifiedButton = document.getElementById( "textified-button" );
+    
     $( '#textified-text' ).toggleClass( 'sr-only' );
+    
+    $( '#textified-arrow' ).toggleClass( 'textified-arrow' );
+    
+  if ( textifiedButton.innerHTML === "Close text version" ) {
+    textifiedButton.innerHTML = "View text version";
+  } else {
+    textifiedButton.innerHTML = "Close text version";
+  } 
   } );
 } )( jQuery );
