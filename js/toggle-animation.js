eba651ea02245bd58c069821a9b0d54cd92c4a16
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-06-15 23:40:37 -0400

    Toggle sr-only class on textified comic

--- js/toggle-animation.js
+++ js/toggle-animation.js
@@ -4,4 +4,8 @@ var jQuery;
   $( '.menu-toggle' ).click( function () {
     $( '#main-menu-icon' ).toggleClass( 'open' );
   } );
+  
+  $( '#textified-toggle' ).click( function () {
+    $( '#textified-text' ).toggleClass( 'sr-only' );
+  } );
 } )( jQuery );
