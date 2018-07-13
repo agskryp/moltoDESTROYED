97baa441edce9baf1fd690a345f1e47ad11bbb54
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-07-13 18:06:26 -0400

    New ad units

--- partials/ads/bottom-of-main-area.php
+++ partials/ads/bottom-of-main-area.php
@@ -1,20 +1,16 @@
-<?php 
-//
-//<div class="ad-container">
-//  <span class="text-center">
-//    Advertisement
-//  </span>
-//  
-//  <ins class="adsbygoogle"
-//       style="display:block"
-//       data-ad-client="ca-pub-5942635838820429"
-//       data-ad-slot="7184708966"
-//       data-ad-format="auto">
-//  </ins>
-//  
-//  <script>
-//    (adsbygoogle = window.adsbygoogle || []).push({});
-//  </script>
-//</div>
-//
-//    
+<div class="ad-container">
+  <span class="text-center">
+    Advertisement
+  </span>
+  
+  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <!-- Bottom of page -->
+  <ins class="adsbygoogle"
+       style="display:block"
+       data-ad-client="ca-pub-5942635838820429"
+       data-ad-slot="2291116454"
+       data-ad-format="auto"></ins>
+  <script>
+  (adsbygoogle = window.adsbygoogle || []).push({});
+  </script>
+</div>
