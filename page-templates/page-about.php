e8d17a78befac83d434c60537b1f085ef6abc998
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-07-13 01:09:12 -0400

    Re-write about page

--- page-templates/page-about.php
+++ page-templates/page-about.php
@@ -24,12 +24,16 @@
   </div>
 
   <p>
-    All site content is created, written, illustrated, and produced by the molto D. stroyed collective. You can get in contact with us using the form below.  We're also available on <a href="<?php echo esc_url( __( 'https://www.facebook.com/molto-DESTROYED-621021044638414/', 'moltodestroyed' ) ); ?>" target="_blank">Facebook</a> and <a href="<?php echo esc_url( __( 'https://twitter.com/moltodestroyed', 'moltodestroyed' ) ); ?>" target="_blank">Twitter</a>.
+    All site content is created, written, illustrated, and produced by cartoonist molto D. stroyed.  Get in touch by using the contact form below.  Also available on <a href="<?php echo esc_url( __( 'https://www.facebook.com/molto-DESTROYED-621021044638414/', 'moltodestroyed' ) ); ?>" target="_blank">Facebook</a> and <a href="<?php echo esc_url( __( 'https://twitter.com/moltodestroyed', 'moltodestroyed' ) ); ?>" target="_blank">Twitter</a> for social messaging and public discourse.
   </p>
   
   <?php require_once get_template_directory() . '/partials/ads/bottom-of-main-area.php'; ?>
 
   <div id="contactForm" class="contact-form">
+    <h2 class="text-center">
+      Contact
+    </h2>
+
     <span class="contact-notice">NOTE: all fields are required</span>
 
     <?php echo do_shortcode( '[contact-form-7 id="1741" title="Contact form 1"]' ); ?>
