f443d45a7506c864d46ebdbdfa4a1e8cf5b22a5b
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-07-15 22:17:36 -0400

    Remove scripts from ads

--- partials/ads/bottom-of-main-area.php
+++ partials/ads/bottom-of-main-area.php
@@ -3,14 +3,9 @@
     Advertisement
   </span>
   
-  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-  <!-- Bottom of page -->
   <ins class="adsbygoogle"
        style="display:block"
        data-ad-client="ca-pub-5942635838820429"
        data-ad-slot="2291116454"
        data-ad-format="auto"></ins>
-  <script>
-  (adsbygoogle = window.adsbygoogle || []).push({});
-  </script>
 </div>
--- partials/ads/top-of-main-area.php
+++ partials/ads/top-of-main-area.php
@@ -1,18 +1,11 @@
-<?php
-//
-//<div class="ad-container">
-//  <span class="text-center">
-//    Advertisement
-//  </span>
-//
-//  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-//  <!-- Top of page -->
-//  <ins class="adsbygoogle"
-//       style="display:block"
-//       data-ad-client="ca-pub-5942635838820429"
-//       data-ad-slot="2359383307"
-//       data-ad-format="auto"></ins>
-//  <script>
-//  (adsbygoogle = window.adsbygoogle || []).push({});
-//  </script>
-//</div>
+<div class="ad-container">
+  <span class="text-center">
+    Advertisement
+  </span>
+
+  <ins class="adsbygoogle"
+       style="display:block"
+       data-ad-client="ca-pub-5942635838820429"
+       data-ad-slot="2359383307"
+       data-ad-format="auto"></ins>
+</div>
