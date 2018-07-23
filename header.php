15d9418648122d16f6b4c403ea3065345dc9eccf
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-07-23 18:15:23 -0400

    Move wp_head above scripts, run function on adsbygoogle error

--- header.php
+++ header.php
@@ -15,15 +15,17 @@
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon.png" />
+  
+  <?php wp_head(); ?>
+  
   <script async src="https://www.googletagmanager.com/gtag/js?id=UA-64750314-1"></script>
-  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" onerror="adsMessage();"></script>
   <script>
        (adsbygoogle = window.adsbygoogle || []).push({
             google_ad_client: "ca-pub-5942635838820429",
             enable_page_level_ads: true
        });
   </script>
-  <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?> >
