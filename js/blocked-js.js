d88e66df1a4f806426de8475db35ba56d17e7ef0
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-07-25 01:00:22 -0400

    Ad blocker message function

--- js/blocked-js.js
+++ js/blocked-js.js
@@ -1,7 +1,7 @@
 var jQuery;
 
-function adsMessage( ) {
-  jQuery( ".ad-container" ).before( '<h1>Uh Oh!!!!</h1>');
-  
+function adsMessage() {
+  jQuery( ".ad-container" ).before(
+    '<div style="text-align: center; margin: 0 auto 1.5em;"><img src="' + moltoConfig.themeDirectory + '/images/messages/blocker-message-01.png" alt="Help support moltoDESTROYED"><img src="' + moltoConfig.themeDirectory + '/images/messages/blocker-message-02.png" alt="Disable your ad-blocker"></div>'
+  );
 } 
-
