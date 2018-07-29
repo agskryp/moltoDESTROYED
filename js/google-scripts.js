6921c7154f2756c33d5959adaa82fe874f888d49
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-07-29 15:23:24 -0400

    Add comic rocket analytics

--- js/google-scripts.js
+++ js/google-scripts.js
@@ -10,3 +10,7 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-NCG5RNG');
+
+// Comic Rocket Analytics
+(function(d,t,p){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.src='http'+(d.location.protocol=='https:'?'s':'')+'://www.comic-rocket.com/metrics.js#'+(p||"");s.parentNode.insertBefore(g,s);
+})(document,'script','key=jkV5');
