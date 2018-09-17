e2be2da3280e8c8a585f1eadc7ad2ac45987abec
Andrew Skrypnyk <a.g.skryp@gmail.com>
2018-09-16 20:01:24 -0400

    Register new nav menu, set it to the footer

--- functions.php
+++ functions.php
@@ -44,6 +44,7 @@ if ( ! function_exists( 'moltodestroyed_setup' ) ) :
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
 			'menu-1' => esc_html__( 'Primary', 'moltodestroyed' ),
+            'menu-2' => esc_html__( 'Footer', 'moltodestroyed' ),
 		) );
 
 		/*
