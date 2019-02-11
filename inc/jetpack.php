81da7faacc35a7c5abb2850c635d90cca33bea36
Andrew Skrypnyk <a.g.skryp@gmail.com>
2019-02-11 11:35:41 -0500

    Kill Infinte Scroll support

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -3,25 +3,16 @@
  * Jetpack Compatibility File
  *
  * @link https://jetpack.com/
- *
  * @package moltodestroyed
  */
 
 /**
  * Jetpack setup function.
  *
- * See: https://jetpack.com/support/infinite-scroll/
  * See: https://jetpack.com/support/responsive-videos/
  * See: https://jetpack.com/support/content-options/
  */
 function moltodestroyed_jetpack_setup() {
-	// Add theme support for Infinite Scroll.
-	add_theme_support( 'infinite-scroll', array(
-		'container' => 'main',
-		'render'    => 'moltodestroyed_infinite_scroll_render',
-		'footer'    => 'page',
-	) );
-
 	// Add theme support for Responsive Videos.
 	add_theme_support( 'jetpack-responsive-videos' );
 
