3541c8e61ba363f75052cdf88013f1a52ae3db63
Andrew Skrypnyk <a.g.skryp@gmail.com>
2019-02-19 12:48:39 -0500

    Update .gitignore to disregard local plugin files

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
-# moltoDESTROYED Wordpress Theme
+# moltoDESTROYED's repository
 
-This is the moltoDESTROYED Wordpress Theme used for the [moltodestoyred.com](https://moltodestroyed.com) web comic.
-
-This theme is not designed to be used as a universal theme and is therefore not recommended to be used as a base theme as it does not meet all of Wordpress' specifications / requirements.
-
-This theme was built using the starter theme `underscores`, you can download and build your own Wordpress Theme using `_s` @ [ https://underscores.me](https://underscores.me)
-
-## Required Plugins
-
-* `Advanced Custom Fields` by Elliot Condon
+This is the repository for the [moltoDESTROYED website](https://moltodestroyed.com).
--- index.php
+++ index.php
@@ -1,46 +1,17 @@
 <?php
-  /**
-   * The main template file
-   *
-   * This is the most generic template file in a WordPress theme
-   * and one of the two required files for a theme (the other being style.css).
-   * It is used to display a page when nothing more specific matches a query.
-   * E.g., it puts together the home page when no home.php file exists.
-   *
-   * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
-   * @package moltodestroyed
-   */
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
-  get_header();
-?>
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
 
-<div id="primary" class="content-area">
-  <?php require_once get_template_directory() . '/partials/ads/top-of-main-area.php'; ?>
-
-  <main id="main" class="site-main blog-posts-list narrow-container">
-    <?php if( have_posts() ) { ?>
-      <h1 class="page-title screen-reader-text">
-        <?php single_post_title(); ?>
-      </h1>
-
-      <?php
-        while ( have_posts() ) {
-          the_post();
-
-          get_template_part( 'template-parts/content', get_post_format() );
-        }
-
-        require_once get_template_directory() . '/partials/ads/bottom-of-main-area.php';
-
-        molto_posts_navigation();
-      }
-
-      else {
-        get_template_part( 'template-parts/content', 'none' );
-      }
-    ?>
-  </main>
-</div>
-
-<?php
-  get_footer();
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
