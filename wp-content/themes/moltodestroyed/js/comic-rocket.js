ec9f2abde0121f1a8098536376015859c5c1c5ad
Andrew Skrypnyk <a.g.skryp@gmail.com>
2019-02-21 16:11:34 -0500

    Use anonymous function

--- wp-content/themes/moltodestroyed/js/comic-rocket.js
+++ wp-content/themes/moltodestroyed/js/comic-rocket.js
@@ -1,9 +1,13 @@
-var jQuery;
+( function($) {
+  $( document ).ready( function() {
+    //var jQuery;
 
-if ( window.innerWidth > 768 ) {
-  jQuery( ".promo-container" ).append( '<div class="text-center" data-comic-rocket-box="728x90"></div>' );
-} 
+    if( window.innerWidth > 768 ) {
+      $( ".promo-container" ).append( '<div class="text-center" data-comic-rocket-box="728x90"></div>' );
+    } 
 
-else {
-  jQuery( ".promo-container" ).append( '<div class="text-center" data-comic-rocket-box="336x280"></div>' );
-}
+    else {
+      $( ".promo-container" ).append( '<div class="text-center" data-comic-rocket-box="336x280"></div>' );
+    }
+  } );
+} )( jQuery );
