16f3457173d3a472d01c5d73d78bab843216f12c
Andrew Skrypnyk <a.g.skryp@gmail.com>
2019-02-22 11:27:23 -0500

    Use specific loader files for mu plugins

---
+++ wp-content/mu-plugins/load-classic-editor.php
@@ -0,0 +1,17 @@
+<?php 
+
+/**
+ * Plugin Name: Classic Editor
+ * Plugin URI:  https://wordpress.org/plugins/classic-editor/
+ * Description: Enables the WordPress classic editor and the old-style Edit Post screen with TinyMCE, Meta Boxes, etc. Supports the older plugins that extend this screen.
+ * Version:     1.3
+ * Author:      WordPress Contributors
+ * Author URI:  https://github.com/WordPress/classic-editor/
+ * License:     GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * Text Domain: classic-editor
+ * Domain Path: /languages
+ * Network:     true
+ */
+
+require WPMU_PLUGIN_DIR . '/classic-editor/classic-editor.php';
