d29d4264c3d9cd7149cca71ad24d39f0e21a1f13
Andrew Skrypnyk <a.g.skryp@gmail.com>
2019-03-08 13:39:19 -0500

    Move blockmessage outside anonymous function

--- wp-content/themes/moltodestroyed/js/extra.js
+++ wp-content/themes/moltodestroyed/js/extra.js
@@ -1,20 +1,24 @@
+/**
+ * Display support images if user is running an ad blocker
+ * Run outside of anonymous loading function to be defined
+ * before googlead script runs
+ *
+ * TODO: add back to anonymous function and async js(?)
+ */
+function blockerMessage() {
+  jQuery( ".ad-container" ).before(
+    '<div style="text-align: center; margin: 0 auto 1.5em;"><img src="' + moltoConfig.themeDirectory + '/images/messages/blocker-message-01.png" alt="Help support moltoDESTROYED"><img src="' + moltoConfig.themeDirectory + '/images/messages/blocker-message-02.png" alt="Disable your ad-blocker"></div>'
+  );
+}
+
 ( function($) {
   $( document ).ready( function() {
-    
-    // Display support images if user is running an ad blocker
-    function blockerMessage() {
-      $( ".ad-container" ).before(
-        '<div style="text-align: center; margin: 0 auto 1.5em;"><img src="' + moltoConfig.themeDirectory + '/images/messages/blocker-message-01.png" alt="Help support moltoDESTROYED"><img src="' + moltoConfig.themeDirectory + '/images/messages/blocker-message-02.png" alt="Disable your ad-blocker"></div>'
-      );
-    }
-    
-    
+        
     // Toggle open class when menu button's clicked
     $( '.menu-toggle' ).click( function () {
       $( '#main-menu-icon' ).toggleClass( 'open' );
     } );
 
-    
     // Toggle comic texted version button
     $( '#textified-toggle' ).click( function () {
       var textifiedButton = document.getElementById( "textified-button" );
