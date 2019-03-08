0eb3248837a7078372dae41932b6e83d8b9e39d3
Andrew Skrypnyk <a.g.skryp@gmail.com>
2019-03-08 13:48:39 -0500

    Kill top of page ad on mobile devices

--- wp-content/themes/moltodestroyed/partials/ads/top-of-main-area.php
+++ wp-content/themes/moltodestroyed/partials/ads/top-of-main-area.php
@@ -1,6 +1,6 @@
 <div class="ad-container">
   <ins id="header-ad"
-       class="adsbygoogle"
+       class="adsbygoogle top-of-page-ad"
        style="display:block"
        data-ad-client="ca-pub-5942635838820429"
        data-ad-slot="2359383307"
