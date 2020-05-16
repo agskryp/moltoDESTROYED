1d509a390d29ee85c79bee35fa4fd0e063b0f4ec
Andrew Skrypnyk <a.g.skryp@gmail.com>
2020-05-16 15:20:18 -0400

    Update Yoast SEO to v14.1

--- wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitf277b5dc76d13dbde08f494c39411183::getLoader();
+return ComposerAutoloaderInit4bf56a192e35dbfaa84a178c46dfe504::getLoader();
