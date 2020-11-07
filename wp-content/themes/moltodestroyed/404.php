3dc5d49576a5140c7be481902cfbfc7ef284d03c
Andrew Skrypnyk <a.g.skryp@gmail.com>
2020-11-06 23:13:09 -0500

    Code clean up, 404 wip

--- wp-content/themes/moltodestroyed/404.php
+++ wp-content/themes/moltodestroyed/404.php
@@ -1,43 +1,37 @@
 <?php
   /**
-   * The template for displaying 404 pages (not found)
-   *
-   * @link https://codex.wordpress.org/Creating_an_Error_404_Page
-   * @package moltodestroyed
+   * The 404 template
    */
 
   get_header();
 ?>
 
-<div id="primary" class="content-area">
-  <main id="main" class="site-main narrow-container">  
-    <section class="error-404 not-found">
-      <h1 class="page-title text-center">4OH4!!</h1>
 
-      <div class="page-content">
-        <p class="text-center h3 bottom-cushion">We're Sorry, but this page doesn't exist.</p>
+<main id="main" class="site-main narrow-container">  
+  <section class="error-404 not-found">
+    <h1 class="page-title text-center">4OH4!!</h1>
 
-        <div class="left-character pull-left">
-          <?php require get_template_directory() . '/partials/random-character-image.php'; ?>
-        </div>
+    <div class="page-content">
+      <p class="text-center h3 bottom-cushion">We're Sorry, but this page doesn't exist.</p>
 
-        <p>It's possible that it did exist once upon a time, and we've decided to change something to inconvience you for a moment.</p>
+      <div class="left-character pull-left">
+        <?php require get_template_directory() . '/partials/random-character-image.php'; ?>
+      </div>
 
-        <div class="right-character pull-right">
-          <?php require get_template_directory() . '/partials/random-character-image.php'; ?>
-        </div>
+      <p>It's possible that it did exist once upon a time, and we've decided to change something to inconvience you for a moment.</p>
 
-        <p>Or maybe it never <em>ever</em> existed!</p>
+      <div class="right-character pull-right">
+        <?php require get_template_directory() . '/partials/random-character-image.php'; ?>
+      </div>
 
-        <p>Perhaps it may exist one day, but today isn't that day. You could always try again tomorrow if you want, we won't stop you.</p>
+      <p>Or maybe it never <em>ever</em> existed!</p>
 
-        <p>In the mean time, you can <a href="<?php echo get_site_url(); ?>">return to the homepage</a>, or see if there's a reason you're seeing this page in one of the <a href="<?php echo get_site_url() . MOLTO_BLOG; ?>">blog postings</a>.</p>
-      </div> 
-    </section>
-  </main>
-</div>
+      <p>Perhaps it may exist one day, but not today. You could always try again tomorrow if you want, we won't stop you.</p>
+
+      <p>In the mean time, you can <a href="<?php echo get_site_url(); ?>">return to the homepage</a>, or see if there's a reason you're seeing this page in one of the <a href="<?php echo get_site_url() . MOLTO_BLOG; ?>">blog postings</a>.</p>
+    </div> 
+  </section>
+</main>
 
 <?php
-  // dynamic_sidebar( 'sidebar-1' ); 
-
   get_footer();
--- wp-content/themes/moltodestroyed/archive.php
+++ wp-content/themes/moltodestroyed/archive.php
@@ -1,53 +1,30 @@
 <?php
   /**
-   * The template for displaying archive pages
+   * The archive template
    *
-   * NOTE:
-   * Using page-archive.php as template for archive lists,
-   * currently NOT using archive.php for anything on the site
-   *
-   * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
-   * @package moltodestroyed
+   * Not currently being utilized
    */
 
   get_header();
 ?>
 
-<div class="narrow-container">
-  <div id="primary" class="content-area">
-    <main id="main" class="site-main">
-      <?php if ( have_posts() ) : ?>
-        <header class="page-header text-center">
-          <?php
-            the_archive_title( '<h1 class="page-title">', '</h1>' );
-              
-            the_archive_description( '<div class="archive-description">', '</div>' );
-          ?>
-        </header>
-
-        <?php
-          while ( have_posts() ) :
-            the_post();
-
-            /*
-             * Include the Post-Format-specific template for the content.
-             * If you want to override this in a child theme, then include a file
-             * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-             */
-            get_template_part( 'template-parts/content', get_post_format() );
-          endwhile;
-        the_posts_navigation();
-
-        else :
-          get_template_part( 'template-parts/content', 'none' );
-      
-        endif;
+<main>
+  <?php if( have_posts() ) { ?>
+    <header>
+      <?php
+        the_archive_title( '<h1>', '</h1>' );
+        the_archive_description( '<div>', '</div>' );
       ?>
-    </main>
-  </div>
+    </header>
 
-  <?php get_sidebar(); ?>      
-</div>
+    <?php
+      while ( have_posts() ) {
+        the_post();
+        the_content();
+      }  
+    }
+  ?>
+</main>
 
 <?php
   get_footer();
