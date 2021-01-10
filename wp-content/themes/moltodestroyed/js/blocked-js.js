27b52a206d6ae7317df194a775f3cc3279d0b566
Andrew Skrypnyk <a.g.skryp@gmail.com>
2021-01-10 18:17:12 -0500

    Kill redundant js files

--- wp-content/themes/moltodestroyed/js/blocked-js.js
+++
@@ -1,9 +0,0 @@
-// var jQuery;
-
-// function blockerMessage() {
-//   jQuery( ".ad-container" ).before(
-//     '<div style="text-align: center; margin: 0 auto 1.5em;"><img src="' + moltoConfig.themeDirectory + '/images/messages/blocker-message-01.png" alt="Help support moltoDESTROYED"><img src="' + moltoConfig.themeDirectory + '/images/messages/blocker-message-02.png" alt="Disable your ad-blocker"></div>'
-//   );
-// } 
-
-// console.log( 'baba' );
