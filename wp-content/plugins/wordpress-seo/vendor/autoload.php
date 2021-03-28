06f7ec77479628bb5e8db01a6b79a28f0ada9b22
Andrew Skrypnyk <a.g.skryp@gmail.com>
2021-03-28 18:23:38 -0400

    Update Yoast SEO

--- wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit4bf56a192e35dbfaa84a178c46dfe504::getLoader();
+return ComposerAutoloaderInit2a05f275a08883e22be3d339dd824174::getLoader();
